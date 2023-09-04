876972787beb93a6b33387c9870fecc107b4a145
Yair Rfz <yairrdz11@gmail.com>
2023-09-04 12:48:06 -0600

    Autor project created

---
+++ StoreService.Api.Autor/Models/AcademicGrade.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreService.Api.Autor.Models
+{
+    public class AcademicGrade
+    {
+        public int AcademicGradeId { get; set; }
+        public string Name { get; set; }
+        public string AcademicCenter { get; set; }
+        public DateTime? GradeDate { get; set; }
+    }
+}
---
+++ StoreService.Api.Autor/Models/AutorBook.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreService.Api.Autor.Models
+{
+    public class AutorBook
+    {
+        public int AutorBookId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Birthdate { get; set; }
+    }
+}
