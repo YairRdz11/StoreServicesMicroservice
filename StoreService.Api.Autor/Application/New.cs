4edb6104aa15e925dcd5c74389d0d9ac9f2e9796
Yair Rfz <yairrdz11@gmail.com>
2023-09-04 21:32:14 -0600

    adding mediatR nuget and creating classes needed

---
+++ StoreService.Api.Autor/Application/New.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace StoreService.Api.Autor.Application
+{
+    public class New
+    {
+        public class Execute : IRequest
+        {
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public DateTime? Birthdate { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Execute>
+        {
+            public Task Handle(Execute request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
