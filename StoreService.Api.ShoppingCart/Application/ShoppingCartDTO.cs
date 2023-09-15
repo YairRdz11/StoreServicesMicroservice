5cc7abd759c3d0c2169a9d200e83f12bc5418373
Yair Rfz <yairrdz11@gmail.com>
2023-09-14 21:30:28 -0600

    Create DTO's for shoppingCart

---
+++ StoreService.Api.ShoppingCart/Application/ShoppingCartDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreService.Api.ShoppingCart.Application
+{
+    public class ShoppingCartDTO
+    {
+        public int ShoppingCartId { get; set; }
+        public DateTime? CreatedDateSession { get; set; }
+        public ICollection<ShoppingCartDTO> Items { get; set; }
+    }
+}
---
+++ StoreService.Api.ShoppingCart/Application/ShoppingDetailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreService.Api.ShoppingCart.Application
+{
+    public class ShoppingDetailDTO
+    {
+        public Guid? BookId { get; set; }
+        public string TitleBook { get; set; }
+        public string AutorBook { get; set; }
+        public DateTime? PublishDate { get; set; }
+    }
+}
